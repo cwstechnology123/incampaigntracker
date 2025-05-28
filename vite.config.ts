81914771546607b011a11786c83c838d20212025
cwstechnology123 <monika.kumari@cwsinfotech.com>
2025-05-28 12:54:08 +0530

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
   optimizeDeps: {
     exclude: ['lucide-react', '@apify/log', 'ansi-colors'],
   },
@@ -19,13 +20,14 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
-    // hmr: {
-    //   clientPort: 443,
-    //   timeout: 120000
-    // },
+    hmr: {
+      clientPort: 443,
+      timeout: 120000
+    },
+    allowedHosts: ['localhost', 'd61d-2401-4900-73e8-946c-c9c-1ba8-e729-cdc2.ngrok-free.app']
   },
   preview: {
     port: 3000,
     host: true,
   },
-})
+});
